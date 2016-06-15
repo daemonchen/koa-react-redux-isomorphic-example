d003514c7f928598653dc7b170e854366f4e4c2a
diego-d5000 <diego-d@outlook.com>
2016-06-15 01:01:26 -0500

    [MOD] server to babel-style

--- src/server/index.js
+++ src/server/index.js
@@ -1,30 +1,24 @@
-'use strict';
-const messages = require('./controllers/messages');
-const compress = require('koa-compress');
-const logger = require('koa-logger');
-const serve = require('koa-static');
-const route = require('koa-route');
-const koa = require('koa');
-const path = require('path');
-const app = module.exports = koa();
+'use strict'
+import Koa from 'koa'
+const app = Koa()
+
+import compress from 'koa-compress'
+import logger from 'koa-logger'
+import serve from 'koa-static'
+import path from 'path'
 
 // Logger
-app.use(logger());
-
-app.use(route.get('/', messages.home));
-app.use(route.get('/messages', messages.list));
-app.use(route.get('/messages/:id', messages.fetch));
-app.use(route.post('/messages', messages.create));
-app.use(route.get('/async', messages.delay));
-app.use(route.get('/promise', messages.promise));
+app.use(logger())
 
 // Serve static files
-app.use(serve(path.join(__dirname, 'public')));
+app.use(serve(path.join(__dirname, 'public')))
 
 // Compress
-app.use(compress());
+app.use(compress())
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log('listening on port 3000');
+  app.listen(3000)
+  console.log('listening on port 3000')
 }
+
+export default app

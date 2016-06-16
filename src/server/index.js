e1e55ab6e3b04be029c60f0c3ac1c201f763bc00
diego-d5000 <diego-d@outlook.com>
2016-06-16 12:43:22 -0500

    [MOD] moved large functions in server index to utils file

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ import path from 'path'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 
-import { Router, RouterContext, match } from 'react-router'
+import { Router, RouterContext } from 'react-router'
 import routes from '../shared/routes';
 
 import { applyMiddleware, createStore } from 'redux'
@@ -22,7 +22,8 @@ import thunkMiddleware from 'redux-thunk'
 import promiseMiddleware from 'redux-promise'
 import combinedReducers from '../shared/reducers'
 
-import fetchComponentData from '../shared/utils/fetchComponentData'
+import { coRouterMatch } from './utils'
+
 const finalCreateStore = applyMiddleware(promiseMiddleware, thunkMiddleware)( createStore )
 
 // Logger
@@ -38,66 +39,11 @@ app.use( function* () {
   const store = finalCreateStore(combinedReducers)
 
   // react-router
-  let finalPage = yield routerMatch(routes, this.url, store)
+  let finalPage = yield coRouterMatch(routes, this.url, store)
 
   this.body = finalPage
 
 })
-
-// ToDo: submodule of this
-function routerMatch(routes, location, store) {
-  return new Promise((resolve, reject) => {
-    match( {routes, location: location}, ( error, redirectLocation, renderProps ) => {
-
-      if ( error ){
-        error.status
-        reject(error)
-      }
-
-      if ( redirectLocation ){
-        let responseValue = {type: 'redirection', url: redirectLocation.pathname + redirectLocation.search}
-        resolve(responseValue)
-      }
-
-      if ( renderProps == null ){
-        let error = {message: 'Not Found', status: 404}
-        reject(error)
-      }
-
-      fetchComponentData( store.dispatch, renderProps.components, renderProps.params)
-
-      const initView = renderToString((
-	  <Provider store={store}>
-	  <RouterContext {...renderProps} />
-	  </Provider>
-      ))
-
-      let state = JSON.stringify( store.getState() );
-
-      let page = renderFullPage( initView, state )
-
-      resolve(page);
-
-    })
-  })
-}
-
-function renderFullPage(html, initialState) {
-  return `
-	<!doctype html>
-	<html lang="utf-8">
-	  <head>
-		<title>Universal Redux Example</title>
-		<link rel="shortcut icon" type="image/png" href="assets/images/react.png">
-	  </head>
-	  <body>
-	  <div class="container">${html}</div>
-		<script>window.$REDUX_STATE = ${initialState}</script>
-		<script src="/js/bundle.js"></script>
-	  </body>
-	</html>
-	`
-}
 
 // Compress
 app.use(compress())
---
+++ src/server/utils/index.js
@@ -0,0 +1,62 @@
+import {match} from 'react-router'
+import fetchComponentData from '../../shared/utils/fetchComponentData'
+
+function renderFullPage(html, initialState) {
+  return `
+    <!doctype html>
+    <html lang="utf-8">
+    <head>
+    <title>Universal Redux Example</title>
+    <link rel="shortcut icon" type="image/png" href="assets/images/react.png">
+    </head>
+    <body>
+    <div class="container">${html}</div>
+    <script>window.$REDUX_STATE = ${initialState}</script>
+    <script src="/js/bundle.js"></script>
+    </body>
+    </html>
+    `
+}
+
+export function coRouterMatch(routes, location, view, store) {
+  if(arguments.length < 2 && arguments[0]){
+    let {routes, location, view, store} = this.arguments
+  }
+  
+  return new Promise((resolve, reject) => {
+    match( {routes, location}, ( error, redirectLocation, renderProps ) => {
+
+      if ( error ){
+        error.status
+        reject(error)
+      }
+
+      if ( redirectLocation ){
+        let responseValue = {type: 'redirection', url: redirectLocation.pathname + redirectLocation.search}
+        resolve(responseValue)
+      }
+
+      if ( renderProps == null ){
+        let error = {message: 'Not Found', status: 404}
+        reject(error)
+      }
+
+      fetchComponentData( store.dispatch, renderProps.components, renderProps.params)
+
+      const initView = renderToString((
+	  <Provider store={store}>
+	  <RouterContext {...renderProps} />
+	  </Provider>
+      ))
+
+      let state = JSON.stringify( store.getState() );
+
+      let page = renderFullPage( initView, state )
+
+      let responseValue = {type:'page', page}
+
+      resolve(responseValue);
+
+    })
+  })
+}
